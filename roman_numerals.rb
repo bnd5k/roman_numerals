d146542c159faa115adf509b1730b20b8d2aeece
Ben Downey <BD@BenDowney.net>
2012-04-22 15:56:47 -0500

    eliminatined duplication in roman to arabic conversion tests

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -17,17 +17,47 @@ class RomanNumeral
 end
 
 class ArabicNumeral
-  def self.convert(letters)
-    answer = 0    
-    digits = { "I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "D" => 500, "M" => 1000 }
+
+digits = { "I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "D" => 500, "M" => 1000 }
+
+  def conv1(letters)
+  	@bump = []    
     my_array = letters.split("") 
-    my_array.each do |let|
-      arabic = digits[let] 
-     answer += arabic
-    end
-    puts answer
-    answer
+    my_array.each_with_index do |let|
+      arabic = digits[let]
+  	puts @bump.join('')
+    @bump << arabic
+    end  
+  end
+
+  def addition(letters)
+    @bump = "II"
+    	if  @bump[index-1] < @bump[index-0] 
+    #maybe have a method for conversion and a method for addition
+    	#	bump[index-0] - bump[indexj-1]
+    		#bump[index-0] = 0 
+    		bump[index+1] = bump[index-1] - bump[index+0] 
+    		mp[index-0] = 0      
+      end
+      #now add that shit up.  
+  end  
+
+  def self.convert(letters)
+    conv1(letters)
+    addition(letters)
+
    end
+   
+#new plan: translate all the letters into numbers,
+#then give a shit about letter location
+#then re-translate letters into numbers
+#then all that shit up
+
+#in order to deal iwth IVs and IX's, I need the program to care about the order in which numers occur.
+#if the current number being evaluated is bigger than the last number being evaluated, then we've got an issue.
+#in short, it must know the most recent number and the current number
+#when the most recent number is not equal to the current number, then...do some cool shit.
+   
 end     
     
     
--- roman_numerals_spec.rb
+++ roman_numerals_spec.rb
@@ -7,148 +7,67 @@ describe RomanNumeral do
     output.should == "nulla"
   end
   
-  it 'converts 1 to I' do
-    output = RomanNumeral.convert(1)
-    output.should == "I"
+  digits = [[1000, "M"],[900, "CM"],[500, "D"],[400, "CD"],[100, "C"],[90, "XC"],[50, "L"], [40, "XL"], [24, "XXIV"],
+          [10, "X"], [9, "IX"], [8, "VIII"], [7, "VII"], [6, "VI"], [5, "V"],  [4, "IV"], [3, "III"], [2, "II"], [1, "I"]]
+    
+  digits.each do |arbc, rmn|
+    it "converts #{arbc} to  #{rmn}" do
+      output = RomanNumeral.convert(arbc)
+      output.should == rmn
+    end  
   end
-  
-  it 'converts 2 to II' do
-    output = RomanNumeral.convert(2)
-    output.should == "II"
-  end  
-  
-  it 'converts 4 to IV' do
-    output = RomanNumeral.convert(4)
-    output.should == "IV"
-  end
-  
-  it 'converts 5 to V' do
-    output = RomanNumeral.convert(5)
-    output.should == "V"
-  end  
-  
-  it 'converts 6 to VI' do
-    output = RomanNumeral.convert(6)
-    output.should == "VI"
-  end
-
-  it 'converts 8 to VIII' do
-    output = RomanNumeral.convert(6)
-    output.should == "VIII"
-  end
-  
-  it 'converts 9 to IX' do
-    output = RomanNumeral.convert(9)
-    output.should == "IX"
-  end  
-  
-  it 'converts 10 to X' do
-    output = RomanNumeral.convert(10)
-    output.should == "X"
-  end  
-  
-  it 'converts 11 to XI' do
-    output = RomanNumeral.convert(11)
-    output.should == "XI"
-  end
-
-  it 'converts 16 to XVI' do
-    output = RomanNumeral.convert(16)
-    output.should == "XVI"
-  end
-  
-  it 'converts 19 to XIX' do
-    output = RomanNumeral.convert(19)
-    output.should == "XIX"
-  end  
-  
-  it 'converts 56 to LVI' do
-    output = RomanNumeral.convert(56)
-    output.should == "LVI"
-  end
-  
-  it 'converts 101 to CI' do
-    output = RomanNumeral.convert(101)
-    output.should == "CI"
-  end
-
-  it 'converts 194 to CXCIV' do
-    output = RomanNumeral.convert(194)
-    output.should == "CXCIV"
-  end
-  
-  it 'converts 491 to CDXCI' do
-    output = RomanNumeral.convert(491)
-    output.should == "CDXCI"
-  end
-      
-  it 'converts 511 to DXI' do
-    output = RomanNumeral.convert(511)
-    output.should == "DXI"
-  end
-
-  it 'converts 1516 to MDXVI' do
-    output = RomanNumeral.convert(1516)
-    output.should == "MDXVI"
-  end  
-  
-  it 'converts 2346 to MMCCCXLVI' do
-    output = RomanNumeral.convert(2346)
-    output.should == "MMCCCXLVI"
-  end 
-
-  it 'converts 4346 to MMMMCCCXLVI' do
-    output = RomanNumeral.convert(4346)
-    output.should == "MMMMCCCXLVI"
-  end 
-
-end  
 
 describe ArabicNumeral do
+ 
+   it 'converts I to 1' do
+     output = ArabicNumeral.convert("I")
+     output.should == 1
+   end 
+   
+   it 'converts II to 2' do
+     output = ArabicNumeral.convert("II")
+     output.should == 2
+   end
+   
+   it 'converts III to 3' do
+     output = ArabicNumeral.convert("III")
+     output.should == 3
+   end
+   
+   it 'converts IV to 4' do
+     output = ArabicNumeral.convert("IV")
+     output.should == 4
+   end  
+   
+   it 'converts V to 5' do
+     output = ArabicNumeral.convert("V")
+     output.should == 5
+   end
+   
+   it 'converts VI to 6' do
+     output = ArabicNumeral.convert("VI")
+     output.should == 6
+   end
+   
+   it 'converts XI to 11' do
+     output = ArabicNumeral.convert("XI")
+     output.should == 11
+   end
+   
+   it 'converts XIV to 14' do
+     output = ArabicNumeral.convert("XIV")
+     output.should == 14
+   end
+   
+   it 'converts XX to 20' do
+     output = ArabicNumeral.convert("XX")
+     output.should == 20
+   end
+ 
+ 
+   it 'converts CII to 102' do
+     output = ArabicNumeral.convert("CII")
+     output.should == 102
+   end
 
-  it 'converts I to 1' do
-    output = ArabicNumeral.convert("I")
-    output.should == 1
-  end 
-  
-  it 'converts II to 2' do
-    output = ArabicNumeral.convert("II")
-    output.should == 2
-  end
-  
-  it 'converts III to 3' do
-    output = ArabicNumeral.convert("III")
-    output.should == 3
-  end
-  
-  it 'converts IV to 4' do
-    output = ArabicNumeral.convert("IV")
-    output.should == 4
-  end  
-  
-  it 'converts V to 5' do
-    output = ArabicNumeral.convert("V")
-    output.should == 5
-  end
-  
-  it 'converts VI to 6' do
-    output = ArabicNumeral.convert("VI")
-    output.should == 6
-  end
-  
-  it 'converts XI to 11' do
-    output = ArabicNumeral.convert("XI")
-    output.should == 11
-  end
-  
-  it 'converts XIV to 14' do
-    output = ArabicNumeral.convert("XIV")
-    output.should == 14
-  end
-  
-  it 'converts XX to 20' do
-    output = ArabicNumeral.convert("XX")
-    output.should == 20
-  end
-    
 end
