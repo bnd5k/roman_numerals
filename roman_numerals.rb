89961f979dc8c8d9ea2bf682bb87645f4b016480
Ben Downey <BD@BenDowney.net>
2012-04-27 10:59:37 -0500

    revised code based on GM's github comments

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,14 +1,17 @@
+DIGITS = { "M" => 1000, "CM" => 900, "D" => 500, "C" => 100, "XC" => 90, "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" =>  4, "I" => 1}  
+
 class RomanNumeral
+  
   def self.convert(num)
     answer = []  
     if num == 0
       "nulla"
     else  
-    digits = [[1000, "M"],[900, "CM"],[500, "D"],[400, "CD"],[100, "C"],[90, "XC"],[50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"],  [4, "IV"], [1, "I"]]        
+       digits = { "M" => 1000, "CM" => 900, "D" => 500, "CD" => 400, "C" => 100, "XC" => 90, "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" =>  4, "I" => 1}
       digits.each do |pairing|
-        while num >= pairing[0]          
-          num = num - pairing[0]
-          answer << pairing[1]
+        while num >= pairing[1]          
+          num = num - pairing[1]
+          answer << pairing[0]
         end
       end
       answer.join('')
@@ -17,7 +20,7 @@ class RomanNumeral
 end
 
 class ArabicNumeral
-
+  
   def self.replace_troublesome_roman_numerals(letters)
    tough_mappings = {"CM" => "DCCCC",  "CD" => "CCCC", "XC" => "LXXXX", "XL" => "XXXX", "IX"=> "VIIII", "IV" => "IIII"}
    tough_mappings.each { |roman, arabic|   letters = letters.gsub(roman, arabic) } 
@@ -25,7 +28,7 @@ class ArabicNumeral
   end
 
   def self.convert_and_add(letters)
-    digits = { "M" => 1000, "CM" => 900, "D" => 500, "C" => 100, "XC" => 90, "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" =>  4, "I" => 1}
+     digits = { "M" => 1000, "CM" => 900, "D" => 500, "C" => 100, "XC" => 90, "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" =>  4, "I" => 1}
     letters = letters.split("")
     letters.inject(0) do |sum, letter|
       arabic = digits[letter]
@@ -35,8 +38,7 @@ class ArabicNumeral
 
   def self.convert(letters)
     roman_string = replace_troublesome_roman_numerals(letters)  
-    arabic_number = convert_and_add(roman_string)
-    arabic_number
+    convert_and_add(roman_string)
   end
   
 end        
